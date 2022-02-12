58d3dae9daed6c0239064b35ca502ddffb28c563
pulsari <brett.bentley7@gmail.com>
2022-02-12 15:20:17 -0800

    Green Buttons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CounterHooks from "./CounterHooks";
 export const ThemeContext = React.createContext();
 
 function App() {
-    const [theme, setTheme] = useState("red");
+    const [theme, setTheme] = useState("green");
     return (
         <ThemeContext.Provider value={{ backgroundColor: theme }}>
             Counter
@@ -17,8 +17,7 @@ function App() {
                     setTheme((prevTheme) => {
                         return prevTheme === "red" ? "blue" : "red";
                     })
-                }
-            >
+                }>
                 Toggle Theme
             </button>
         </ThemeContext.Provider>
